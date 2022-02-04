61e7d7e7f4f9035c8c14f5d4ad33f4b7b2d0d364
Zacharia Njiraini <zjerainy@gmail.com>
2022-02-04 11:48:44 +0300

    Added two cars and updated mt_rand(0, count())

--- loops.php
+++ loops.php
@@ -42,10 +42,12 @@ while ($num <= 10) {
     } while ($y <= 60);
     // .switch
     echo "<h3 class = 'myheadings'>Switch statement for random car type</h3>  ";
+
     // ADD CARS HERE 
-    $mycars = array("limousine", "convertible", "minivan", "pickup", "sports car", "Formula1 car", "sedan", "landrover defender", "probox", "nduthi", "peugeout");
+    $mycars = array("limousine", "convertible", "minivan", "pickup", "sports car", "Formula1 car", "sedan", "landrover defender", "probox", "nduthi", "peugeout", "mazda", "jeep");
+
     // ADD the length or random numbers according to the cars added
-    $mycar = $mycars[mt_rand(0, 10)];
+    $mycar = $mycars[mt_rand(0, count($mycars))];
     switch ($mycar) {
         case 'limousine':
             echo "<p class='mystyles car'>You have a $mycar , keep the drinks coming🍾</p>";
